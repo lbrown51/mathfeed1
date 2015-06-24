4b79443b8a453e13c90b595770ce635c6f09dc20
lbrown51 <lbrown51@u.rochester.edu>
2015-06-24 02:52:39 -0400

    Adding to database for problems works fine. They display themsevles nicely. Must remember that I intend to use #b53f51 for my third color to complement the other two

--- client/get/problem/problem.js
+++ client/get/problem/problem.js
@@ -3,3 +3,9 @@ Template.problem.events({
      //Meteor.call("insertProblem"  
     }
 });
+
+Template.problem.onRendered(function(){
+     $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
